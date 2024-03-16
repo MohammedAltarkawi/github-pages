253c78bfbf5b798347321894132a99ea7ded27bb
MohammedAltarkawi <mohammed.alsaloum.altarkawi@msg.group>
2024-03-16 19:18:31 +0100

    test self cont

--- dist/controller/Main-dbg.controller.js
+++ dist/controller/Main-dbg.controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-sap.ui.define(["sap/m/MessageBox", "./BaseController", "sap/ui/core/Theming"], function (MessageBox, __BaseController, Theming) {
+sap.ui.define(["sap/m/MessageBox", "./BaseController", "sap/ui/core/Theming", "sap/ui/core/Fragment"], function (MessageBox, __BaseController, Theming, Fragment) {
   "use strict";
 
   function _interopRequireDefault(obj) {
@@ -30,7 +30,32 @@ sap.ui.define(["sap/m/MessageBox", "./BaseController", "sap/ui/core/Theming"], f
       } else {
         Theming.setTheme(this._sLightTheme);
       }
-    }
+    },
+    onOpenDialog: function _onOpenDialog(oEvent) {
+      const oButton = oEvent.getSource();
+      // create dialog lazily
+      if (!this.dialogPromise) {
+        this.dialogPromise = this.loadFragment({
+          name: 'mst.githubpage.fragments.InfoPopover'
+        });
+      }
+      this.dialogPromise.then(function (oDialog) {
+        debugger;
+        oDialog.openBy(oButton);
+      });
+    },
+    handlePopoverPress: async function _handlePopoverPress(oEvent) {
+      const oButton = oEvent.getSource(),
+        oView = this.getView();
+      const oPopover = await Fragment.load({
+        id: oView.getId(),
+        name: 'mst.github.view.fragments.InfoPopover',
+        controller: this
+      });
+      oView.addDependent(oPopover);
+      oPopover.openBy(oButton);
+    },
+    _getInfoPopover: function _getInfoPopover() {}
   });
   return Main;
 });
--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -3,6 +3,10 @@ import BaseController from "./BaseController";
 import Theming from "sap/ui/core/Theming";
 import Event from "sap/ui/base/Event";
 import Switch from "sap/m/Switch";
+import Button from "sap/m/Button";
+import Fragment from "sap/ui/core/Fragment";
+import Popover from "sap/m/Popover";
+import Dialog from "sap/m/Dialog";
 
 /**
  * @namespace mst.githubpage.controller
@@ -11,6 +15,7 @@ export default class Main extends BaseController {
 
 	private _sLightTheme = "sap_horizon";
 	private _sDarkTheme = "sap_horizon_dark";
+	private dialogPromise: Promise<Popover>;
 
 	public sayHello(): void {
 		MessageBox.show("Hello World!");
@@ -28,5 +33,38 @@ export default class Main extends BaseController {
 		} else {
 			Theming.setTheme(this._sLightTheme);
 		}
+	}
+
+
+	onOpenDialog(oEvent: Event): void {
+		const oButton = oEvent.getSource() as Button
+		// create dialog lazily
+			if (!this.dialogPromise) {
+				this.dialogPromise = <Promise<Popover>> this.loadFragment({
+					name: 'mst.githubpage.fragments.InfoPopover'
+				});
+			} 
+			this.dialogPromise.then(function(oDialog) {
+				debugger
+				oDialog.openBy(oButton)
+			});
+		}   
+
+	public async handlePopoverPress(oEvent: Event){
+		const oButton = oEvent.getSource() as Button,
+		oView = this.getView();
+
+		const oPopover = (await Fragment.load({
+            id: oView.getId(),
+            name: 'mst.github.view.fragments.InfoPopover',
+            controller: this
+        })) as Popover;
+
+		oView.addDependent(oPopover)
+		oPopover.openBy(oButton)
+	}
+
+	private _getInfoPopover(){
+		
 	}
 }
